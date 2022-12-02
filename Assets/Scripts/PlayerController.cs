1aa494fedddaa3187fdd98c688510c61460abe1e
stopresent <mamy4938@gmail.com>
2022-12-02 15:16:20 +0900

    player 이동

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float _speed = 10.0f;
+
+    void Start()
+    {
+        Managers mg = Managers.Instance;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+    }
+}
