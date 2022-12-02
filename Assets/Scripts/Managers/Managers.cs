32cbbae1b1b397c354c6ef5cbcf70c0e09b8f14b
stopresent <mamy4938@gmail.com>
2022-12-02 14:00:36 +0900

    매니저 만들기

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloUnity : MonoBehaviour
+public class Managers : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity");
-        Debug.Log("test1");
+        
     }
 
     // Update is called once per frame
