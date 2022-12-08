b36c863a38c5842eeca8f63a2302e2aaa06698ae
stopresent <mamy4939@gmail.com>
2022-12-08 19:59:54 +0900

    UI END

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -13,8 +13,8 @@ public class PlayerController : MonoBehaviour
     {
         Managers.Input.MouseAction -= OnMouseClicked;
         Managers.Input.MouseAction += OnMouseClicked;
-        
-        Managers.UI.ShowPopupUI<UI_Button>();
+
+        Managers.UI.ShowSceneUI<UI_Inven>();
     }
 
     public enum PlayerState
