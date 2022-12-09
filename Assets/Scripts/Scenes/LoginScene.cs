cb0a39c4010e1a7bde325c81f4d5a7bb8630206f
stopresent <mamy4939@gmail.com>
2022-12-09 12:02:21 +0900

    Scene Manager END

---
+++ Assets/Scripts/Scenes/LoginScene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginScene : BaseScene
+{
+
+    protected override void Init()
+    {
+        base.Init();
+
+        SceneType = Define.Scene.Login;
+
+        // Managers.UI.ShowSceneUI<UI_Inven>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Managers.Scene.LoadScene(Define.Scene.Game);
+        }
+    }
+
+    public override void Clear()
+    {
+        Debug.Log("Login Scene Clear!");
+    }
+
+
+}
