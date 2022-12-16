f1a7dfd2e4f53a2500ffccdfb675254f255ffa89
stopresent <mamy4939@gmail.com>
2022-12-16 14:38:05 +0900

    DataManager

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-
+   
     protected override void Init()
     {
         base.Init();
@@ -12,6 +12,8 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game;
 
         Managers.UI.ShowSceneUI<UI_Inven>();
+
+        Dictionary<int, Stat> dict = Managers.Data.StatDict;
     }
 
     public override void Clear()
